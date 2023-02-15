fba6b53d04b86a461f1d2cf5177c310dd5908745
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2023-02-14 22:53:12 -0700

    adds animal and dog class

---
+++ interfacesLab/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfacesLab
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Dogs eat meat.");
+        }
+    }
+}
